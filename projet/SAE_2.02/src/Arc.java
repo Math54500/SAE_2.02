cd426e798f6002e5d2e61d985bf6f3b3326bc138
Mathias Soret <Maths54500z@gmail.com>
2023-05-19 17:16:57 +0200

    question 4

--- projet/SAE_2.02/src/Arc.java
+++ projet/SAE_2.02/src/Arc.java
@@ -1,2 +1,14 @@
 public class Arc {
+
+    String dest;
+    double cout;
+
+    Arc(String d, double c){
+        this.dest = d;
+        if (c > 0){
+            this.cout = c;
+        }else {
+            this.cout = 0;
+        }
+    }
 }
